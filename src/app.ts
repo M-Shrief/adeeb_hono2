2cad982bdd4a052919a6dfeade96ddc94dc1aca1
m.shrief <m.shrief.dev@proton.me>
2026-06-20 11:17:04 +0300

    Added route validation using valibot, added openapi & scalar routes documentation using hono-openapi

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 import { Hono } from "hono";
+import {
+  validator as vValidator,
+  resolver,
+  describeRoute,
+  openAPIRouteHandler
+} from "hono-openapi";
+import * as v from "valibot";
+import { Scalar } from '@scalar/hono-api-reference'
 // Middlewares
 import { logger as loggerMiddleware } from 'hono/logger';
 import { secureHeaders } from 'hono/secure-headers';
 import { cors } from 'hono/cors';
 import { compress } from 'hono/compress';
 
-
 const app = new Hono();
 
 
+const base_response = v.object({message: v.string()})
 
 app.use(loggerMiddleware());
 app.use(secureHeaders());
@@ -18,6 +26,25 @@ app.use(compress());
 
 app.get(
   '/', 
+  describeRoute({
+    operationId: "index",
+    responses: {
+      200: {
+        description: "Successful response",
+        content: {
+          "application/json": {
+            schema: resolver(v.object({
+              title: v.string(),
+              description: v.string(),
+              version: v.string(),
+              docs: v.string(),
+              openapi: v.string()
+            })),
+          },
+        },
+      },
+    },
+  }),
   (c) => {
   var project_metadata = {
     title: "Adeeb Hono",
@@ -32,10 +59,70 @@ app.get(
 
 app.get(
   '/ping', 
+  describeRoute({
+    responses: {
+      200: {
+        description: "Successful response",
+        content: {
+          "application/json": {
+            schema: resolver(base_response),
+          },
+        },
+      },
+    },
+  }),
   async (c) => {
   c.status(200)
   return c.json({message: 'pong'});
 });
 
+
+
+app.get(
+  "/openapi.json",
+  describeRoute({
+    responses: {
+      200: {
+        description: "Get OpenAPI spec",
+      },
+    },
+  }),
+  openAPIRouteHandler(app, {
+    documentation: {
+      info: {
+        title: "Adeeb Hono",
+        description: "Adeeb's Backend iteration in JS & TS, using Hono",
+        version: "0.1.0",
+      },
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
+        },
+      }
+    },
+    includeEmptyPaths: true
+  }),
+);
+
+app.get(
+  '/docs',
+  describeRoute({
+    responses: {
+      200: {
+        description: "Scalar docs",
+      },
+    },
+  }),
+  Scalar({
+    title: "Adeeb Hono",
+    url:   "/openapi.json",
+    // theme: 'purple',
+    darkMode: true
+  })
+)
 
 export { app }
