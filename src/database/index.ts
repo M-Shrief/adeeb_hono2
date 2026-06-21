4fd22d7861e2ea232cc399ad3e3d772bc6a81e9c
m.shrief <m.shrief.dev@proton.me>
2026-06-21 23:34:05 +0300

    Added schemas to drizzle instance to be able to use non-sql methods.

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,10 +2,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { MigrationConfig } from 'drizzle-orm/migrator';
 /////
 import {DB_URL, DB_MIGRATION_FOLDER} from "../config.js"
-
+import * as schemas from "./schemas.js"
 
 
 export const db = drizzle({ 
+  schema: schemas,
   connection: { 
     url: DB_URL, 
     ssl: false 
