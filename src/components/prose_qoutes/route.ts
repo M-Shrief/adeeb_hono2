8cccb88b14c684f0c41045cf75d34f59f3f45a92
m.shrief <m.shrief.dev@proton.me>
2026-06-23 11:17:34 +0300

    Added POST /prose_qoutes/many

--- src/components/prose_qoutes/route.ts
+++ src/components/prose_qoutes/route.ts
@@ -6,7 +6,7 @@ import { sql, getTableColumns, eq } from 'drizzle-orm';
 /////
 import { db } from "../../database/index.js"
 import { prose_qoutes_table } from "../../database/schemas.js"
-import { create_one_req, create_one_res } from './schema.js'
+import { create_many_req, create_many_res, create_one_req, create_one_res } from './schema.js'
 ///// Utils
 import { id_param_validator, json_validator, query_validator } from '../../utils/validators.js'
 import { HttpStatusCode, base_response_schema, queries_schema_for_get_all_req, get_described_route, get_all_schema } from '../../utils/api.js';
@@ -45,6 +45,34 @@ prose_qoute_route.post(
             return c.json(new_prose_qoute, HttpStatusCode.CREATED)
         } catch(e) {
             logger.error({error:e}, "Error: create ProseQoute")
+            return c.json({message: "Unknown error, try again later"}, HttpStatusCode.BAD_REQUEST)
+        }
+    }
+)
+
+prose_qoute_route.post(
+    "/prose_qoutes/many",
+    describeRoute({
+        tags: ["ProseQoutes"],
+        summary: "Create Many",
+        responses: {
+           ...get_described_route(HttpStatusCode.OK, "Successful response", create_many_res),
+           ...get_described_route(HttpStatusCode.BAD_REQUEST, "Bad Request", base_response_schema)
+        },
+    }),
+    json_validator(create_many_req, "Invalid data, can't be used to create many ProseQoutes"),
+    async (c) => {
+        try {
+            let new_data = await c.req.json()
+            let new_prose_qoutes = await db
+                .insert(prose_qoutes_table)
+                .values(new_data)
+                // .onConflictDoNothing()
+                .returning()
+
+            return c.json({created_items: new_prose_qoutes, success_count: new_prose_qoutes.length, failed_count: new_data.length - new_prose_qoutes.length}, HttpStatusCode.CREATED)
+        } catch(e) {
+            logger.error({error:e}, "Error: creating multiple ProseQoutes")
             return c.json({message: "Unknown error, try again later"}, HttpStatusCode.BAD_REQUEST)
         }
     }
