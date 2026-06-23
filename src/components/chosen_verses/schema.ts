3fd62694333ec1036e0254f872ce5d6062f4b9b5
m.shrief <m.shrief.dev@proton.me>
2026-06-23 10:53:04 +0300

    Added PUT /chosen_verses/:id

--- src/components/chosen_verses/schema.ts
+++ src/components/chosen_verses/schema.ts
@@ -52,3 +52,13 @@ export const create_many_res = object({
   success_count: number(),
   failed_count: number(),
 })
+
+export const update_req = object({
+  tags: optional(tags_schema),
+  verses: optional(verses_schema),
+  is_couplet: optional(is_couplet_schema),
+  reviewed: optional(reviewed_schema),
+
+  adeeb_id: optional(uuid_schema),
+  poem_id: optional(uuid_schema),
+});
