f89655486dbe0075014291e2129ae986cbeb1491
m.shrief <m.shrief.dev@proton.me>
2026-06-24 19:40:25 +0300

    Added auth utils to sign & verify JWT token with it's payload.

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -15,3 +15,65 @@ export const compare_password = async (password: string, pass_hash: string) =>
   await bcrypt.compare(password, pass_hash);
 
 
+const PERMISSIONS = {
+    WRITE: "write",
+    READ: "read"
+} as const
+type PERMISSION = typeof PERMISSIONS[keyof typeof PERMISSIONS];
+
+
+type RoleEnumType = typeof RoleEnum[keyof typeof RoleEnum];
+
+export function create_permission(role: RoleEnumType, op: PERMISSION = PERMISSIONS.READ): string {
+    return role + ":" + op
+}
+
+export function create_permissions(roles: RoleEnumType[]) {
+    let permissions: string[] = []
+
+    for(let role of roles) {
+        let read_permission = create_permission(role, PERMISSIONS.READ)
+        permissions.push(read_permission)
+
+        let write_permission = create_permission(role, PERMISSIONS.WRITE)
+        permissions.push(write_permission)
+    }
+
+    return permissions
+}
+
+
+export const sign_token = async (id: string, username: string, roles: RoleEnumType[], exp_after_hours: number = 2) => {
+    let user = {
+        id,
+        username,
+        roles
+    }
+    let permissions = create_permissions(roles)
+
+    return await sign(
+        {
+        ...user,
+        permissions,
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 * exp_after_hours, // default is 2 Hours.
+        iat: Math.floor(Date.now() / 1000),
+        },
+        JWT_PRIVATE_KEY,
+        'RS256',
+    );
+};
+
+export const verify_token = async (auth_header: string): Promise<JWTPayload | null> => {
+    try {
+        let token = auth_header.slice(7)
+
+        let payload =  await verify(token, JWT_PUBLIC_KEY, "RS256") 
+
+        return payload
+    } catch(e) {
+        console.error(e)
+        return null
+    }
+}
+
+
