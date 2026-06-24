b194be42712aec8da86b2ed3a174966aa46de52d
m.shrief <m.shrief.dev@proton.me>
2026-06-24 19:45:43 +0300

    Forgot to commit config.ts and .env.example after adding auth utils, added JWT public & private keys to config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 import { loadEnvFile } from 'node:process';
-
+import fs from 'node:fs';
+// utils
+import { logger } from './utils/logger.js';
 
 
 loadEnvFile('.env');
@@ -16,3 +18,18 @@ export const {
 export const DB_URL = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
 export const NODE_ENV = process.env.NODE_ENV as "dev" | "prod"
+
+
+export let JWT_PRIVATE_KEY: string = '';
+export let JWT_PUBLIC_KEY: string = '';
+
+if (process.env.JWT_PRIVATE_PATH && process.env.JWT_PUBLIC_PATH) {
+  JWT_PRIVATE_KEY = fs
+    .readFileSync(process.env.JWT_PRIVATE_PATH!)
+    .toString()
+    .trim();
+  JWT_PUBLIC_KEY = fs.readFileSync(process.env.JWT_PUBLIC_PATH!).toString().trim();
+} else {
+  console.error('JWT Private key is not defined');
+  console.error('JWT Public key is not defined');
+}
