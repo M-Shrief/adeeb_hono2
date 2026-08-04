e8f6c4f411f36e65b52a2c780f0a2d7328eb25cb
m.shrief <m.shrief.dev@proton.me>
2026-08-04 11:43:46 +0300

    Checked for admin authority in POST/PUT/DELETE requests in Adeebs component

--- src/components/adeebs/route.ts
+++ src/components/adeebs/route.ts
@@ -9,9 +9,10 @@ import { adeeb_table } from "../../database/schemas.js"
 import { one_schema, create_many_req, create_many_res, create_one_req, create_one_res, update_req } from './schema.js'
 import { cache_del, cache_get, cache_set, format_key_by_id } from "../../cache/utils.js"
 ///// Utils
-import { id_param_validator, json_validator, query_validator } from '../../utils/validators.js'
-import { HttpStatusCode, base_response_schema, queries_schema_for_get_all_req, get_described_route, get_all_schema } from '../../utils/api.js';
+import { auth_header_validator, id_param_validator, json_validator, query_validator } from '../../utils/validators.js'
+import { HttpStatusCode, base_response_schema, queries_schema_for_get_all_req, get_described_route, get_all_schema, describe_jwt_security } from '../../utils/api.js';
 import { logger } from '../../utils/logger.js';
+import { verify_adminstrator } from '../../utils/auth.js';
 
 export const adeeb_route = new Hono()  
 
@@ -113,12 +114,15 @@ adeeb_route.post(
     describeRoute({
         tags: ["Adeebs"],
         summary: "Create One",
+        ...describe_jwt_security,
         responses: {
            ...get_described_route(HttpStatusCode.OK, "Successful added Adeeb", create_one_res),
            ...get_described_route(HttpStatusCode.CONFLICT, "Adeeb already exists", base_response_schema),
            ...get_described_route(HttpStatusCode.BAD_REQUEST, "Bad Request", base_response_schema),
         },
     }),
+    auth_header_validator(),
+    verify_adminstrator(),
     json_validator(create_one_req, "Invalid data for Adeeb"),
     async(c) => {
         try {
@@ -148,11 +152,14 @@ adeeb_route.post(
     describeRoute({
         tags: ["Adeebs"],
         summary: "Create Many",
+        ...describe_jwt_security,
         responses: {
            ...get_described_route(HttpStatusCode.OK, "Successful response", create_many_res),
            ...get_described_route(HttpStatusCode.BAD_REQUEST, "Bad Request", base_response_schema)
         },
     }),
+    auth_header_validator(),
+    verify_adminstrator(),
     json_validator(create_many_req, "Invalid data, can't be used to create many Adeebs"),
     async (c) => {
         try {
@@ -176,11 +183,14 @@ adeeb_route.put(
     describeRoute({
         tags: ["Adeebs"],
         summary: "Update One",
+        ...describe_jwt_security,
         responses: {
            ...get_described_route(HttpStatusCode.NO_CONTENT, "Updated Successfully"),
            ...get_described_route(HttpStatusCode.BAD_REQUEST, "Bad Request, try again later.", base_response_schema),
         },
     }),
+    auth_header_validator(),
+    verify_adminstrator(),
     id_param_validator(),
     json_validator(update_req, "Invalid data for update"),
     async(c) => {
@@ -207,11 +217,14 @@ adeeb_route.delete(
     describeRoute({
         tags: ["Adeebs"],
         summary: "Delete One",
+        ...describe_jwt_security,
         responses: {
            ...get_described_route(HttpStatusCode.NO_CONTENT, "Deleted Successfully"),
            ...get_described_route(HttpStatusCode.BAD_REQUEST, "Bad Request, try again later.", base_response_schema),
         },
     }),
+    auth_header_validator(),
+    verify_adminstrator(),
     id_param_validator(),
     async (c) => {
         try {
